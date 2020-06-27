a3f073262bda57fb0b5c97598d2977802f2c01ef
Adnan Niaz <adnanniaz77@yahoo.com>
2020-06-27 06:33:10 -0700

    modified README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 <h1>User Authorization</h1>
-<h2>Node.js, Express, MongoDB</h2>
+<h2>Node.js, Express, MongoDB, @hapi/joi, bcrypt and JWT</h2>
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,16 @@
 const router = require("express").Router();
 const User = require("../model/User");
-const Joi = require('@hapi/joi');
 
 // Validate user input / submission
+const Joi = require("@hapi/joi");
 const schema = Joi.object({
     name: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-})
-
+    password: Joi.string().min(6).required(),
+});
 
 // Register new user
 router.post("/register", async (req, res) => {
-
     // Applying validation
     const { error } = schema.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);

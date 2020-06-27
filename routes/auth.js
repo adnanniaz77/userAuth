c718b75d3a6be87bd4fecce04fef2eadd0d3194c
Adnan Niaz <adnanniaz77@yahoo.com>
2020-06-27 10:49:56 -0700

    created functions for registerValidation and loginValidation also added condion to check if user already exists before creating a new one

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,39 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
-// Validate user input / submission
+// Validate user input
 const Joi = require("@hapi/joi");
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
-});
+const { schema } = require("../model/User");
+
+// function for register validation
+const registerValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+    });
+    return schema.validate(data);
+};
+
+// function for login validation
+const loginValidation = (data) => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required,
+    });
+
+    return schema.validate(data);
+};
 
 // Register new user
 router.post("/register", async (req, res) => {
     // Applying validation
-    const { error } = schema.validate(req.body);
+    const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
+
+    // Checking if user already exists
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).send("Email already exists");
 
     const user = new User({
         name: req.body.name,
